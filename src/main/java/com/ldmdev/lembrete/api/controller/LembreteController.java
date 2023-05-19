a0a76876eff3268af573084b013122f3c693d01f
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2023-05-19 02:44:26 -0300

    Povoando BD com Jakarta

--- src/main/java/com/ldmdev/lembrete/api/controller/LembreteController.java
+++ src/main/java/com/ldmdev/lembrete/api/controller/LembreteController.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,26 +14,17 @@ import com.ldmdev.lembrete.domain.model.Lembrete;
 
 /**
  * 
- * 
- * 
  **/
 @RestController
 public class LembreteController {
 	
-	@GetMapping("/clientes")
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@GetMapping("/lembretes")
 	public List<Lembrete> listar() {
 		
-		Lembrete lembrete1 = new Lembrete();
-		lembrete1.setId(1L);
-		lembrete1.setNome("Dormir");
-		lembrete1.setData(new Date());
-		
-		Lembrete lembrete2 = new Lembrete();
-		lembrete2.setId(2L);
-		lembrete2.setNome("Dormi maisr");
-		lembrete2.setData(new Date());
-		
-		
-		return Arrays.asList(lembrete1,lembrete2);
+		return manager.createQuery("from Lembrete", Lembrete.class)
+				.getResultList();
 	}
 }
--- src/main/java/com/ldmdev/lembrete/domain/model/Lembrete.java
+++ src/main/java/com/ldmdev/lembrete/domain/model/Lembrete.java
@@ -1,15 +1,30 @@
 package com.ldmdev.lembrete.domain.model;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name="lembretes")
 public class Lembrete {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
+	
 	private Date data;
 	
 	
